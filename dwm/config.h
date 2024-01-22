6bbcf401f8bd0707b9cc84c45722c0f393cdc48e
bergoid <bergoid@users.noreply.github.com>
2024-01-22 09:57:54 +0100

    reboot/shutdown

--- dwm/config.h
+++ dwm/config.h
@@ -60,6 +60,10 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+// The 'dwm' user needs to have NOPASSWD 'sudo' access to the 'shutdown' command:
+static const char *rebootcmd[]  = { "sudo", "shutdown", "-r", "now", NULL };
+static const char *shutdowncmd[]  = { "sudo", "shutdown", "-h", "now", NULL };
+
 static Key keys[] = {
     /* modifier                     key        function        argument */
     { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -67,6 +71,8 @@ static Key keys[] = {
 // Keep 'Alt + b' keyboard shortcut for bash:
 //    { MODKEY,                       XK_b,      togglebar,      {0} },
 // Use 'Alt + spacebar' instead:
+    { MODKEY|ShiftMask|ControlMask, XK_r,      spawn,          {.v = rebootcmd } },
+    { MODKEY|ShiftMask|ControlMask, XK_s,      spawn,          {.v = shutdowncmd } },
     { MODKEY,                       XK_space,  togglebar,      {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
